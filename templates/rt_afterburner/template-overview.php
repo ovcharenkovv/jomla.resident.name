3c020b80bf0b2a09e65380a7dfc7aa8cf989cfa2
Ovcharenko Volodymyr <vova.o@masterofcode.com>
2012-05-28 20:18:14 +0300

    add fast template and start slicing

---
+++ templates/rt_afterburner/template-overview.php
@@ -0,0 +1,10 @@
+<h1>RocketTheme Afterburner</h1>
+
+<img src="../templates/rt_afterburner/template_thumbnail.png" style="border: 4px solid #ccc; padding: 2px; float: left; margin-bottom: 10px;" /><div class="clr"></div><br />Afterburner is a lightning fast GPL template designed specifically for speed and SEO. For additional information visit the <a href="http://www.rockettheme.com/joomla-templates/afterburner">Afterburner Overview Page</a>.
+
+<h3>Changelog</h3>
+
+<b>1.6.0</b><br />
+<ul>
+  <li>Initial Release</li>
+</ul>
