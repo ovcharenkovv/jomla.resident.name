c31d90e37f88ba237b97b79f5aa55e07651cee76
Vladimir Ovcharenko <ovcharenkovv@gmail.com>
2012-06-01 01:52:08 +0300

    dump db

--- configuration.php
+++ configuration.php
@@ -49,10 +49,10 @@ class JConfig {
 	public $MetaAuthor = '1';
 	public $MetaVersion = '0';
 	public $robots = '';
-	public $sef = '0';
-	public $sef_rewrite = '0';
-	public $sef_suffix = '0';
-	public $unicodeslugs = '0';
+	public $sef = '1';
+	public $sef_rewrite = '1';
+	public $sef_suffix = '1';
+	public $unicodeslugs = '1';
 	public $feed_limit = '10';
 	public $log_path = '/var/www/resident.name/logs';
 	public $tmp_path = '/var/www/resident.name/tmp';
